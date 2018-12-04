b26db0be1f9dfe065dc698fa88f82a19d936e739
Eric Evans <eevans@wikimedia.org>
2018-12-04 11:47:01 -0600

    simplify barebones WSGI application

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-import json
 
-MSG = json.dumps({'value': 'Hello, world!'}).encode('utf-8')
+# Barebones WSGI application.
+
+import json
 
 
 def application(env, start_response):
-    path = env['PATH_INFO']
-    if path == '/':
-        start_response('200 OK', [('Content-Type', 'application/json')])
-        return [MSG]
-
-    start_response('404 Not Found', [('Content-Type', 'text/plain')])
-    return [b'Not Found']
+    start_response("200 OK", [("Content-Type", "text/plain")])
+    return [b"Hello World!"]
